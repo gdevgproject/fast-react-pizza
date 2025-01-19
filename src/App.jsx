f968991ef5b3cd423e54e4374c1f438f7534595c
gdevgproject <gdev.gproject@gmail.com>
2025-01-19 21:20:30 +0700

    5.4 Sử dụng errorElement property, useRouteError

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Home from './ui/Home'
-import Menu, { loader as menuLoader } from './features/menu/Menu'
+import { RouterProvider, createBrowserRouter } from 'react-router'
 import Cart from './features/cart/Cart'
+import Menu, { loader as menuLoader } from './features/menu/Menu'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
 import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
+import Home from './ui/Home'
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       { path: '/', element: <Home /> },
       {
         path: '/menu',
         element: <Menu />,
-        loader: menuLoader
+        loader: menuLoader,
+        errorElement: <Error />
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
