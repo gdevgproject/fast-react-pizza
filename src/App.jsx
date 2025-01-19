6b53f2ddcfb6b30c165de816be3ee36aae5decaf
gdevgproject <gdev.gproject@gmail.com>
2025-01-19 23:00:31 +0700

    6 Component Search and useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router'
 import Cart from './features/cart/Cart'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import CreateOrder from './features/order/CreateOrder'
-import Order from './features/order/Order'
+import Order, { loader as orderLoader } from './features/order/Order'
 import AppLayout from './ui/AppLayout'
 import Error from './ui/Error'
 import Home from './ui/Home'
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
-      { path: '/order/:orderId', element: <Order /> }
+      { path: '/order/:orderId', element: <Order />, loader: orderLoader, errorElement: <Error /> }
     ]
   }
 ])
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -3,7 +3,6 @@ import { useNavigate, useRouteError } from 'react-router'
 function Error() {
   const navigate = useNavigate()
   const error = useRouteError()
-  console.log(error)
 
   return (
     <div>
