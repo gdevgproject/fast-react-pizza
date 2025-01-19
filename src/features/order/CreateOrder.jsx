1ca7905375ab9ff60b62a3aaa8f7853e0bae7ac3
gdevgproject <gdev.gproject@gmail.com>
2025-01-19 23:47:14 +0700

    wip: Create Order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Form } from 'react-router'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str)
@@ -35,7 +35,8 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      {/* <Form method='POST' action='/order/new'> */}
+      <Form method='POST'>
         <div>
           <label>First Name</label>
           <input type='text' name='customer' required />
@@ -69,9 +70,11 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   )
 }
+
+export function action() {}
 
 export default CreateOrder
