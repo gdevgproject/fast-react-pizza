b31446ce04b70adf757e9e2b7b18bdf8cdeec0f9
gdevgproject <gdev.gproject@gmail.com>
2025-01-26 00:14:26 +0700

    8.2 styling text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router'
 import SearchOrder from '../features/order/SearchOrder'
+import Username from '../features/users/Username'
 
 export default function Header() {
   return (
-    <header className='bg-yellow-500'>
-      <Link to='/'>Fast React Pizza Co.</Link>
+    <header className='bg-yellow-500 uppercase'>
+      <Link to='/' className='tracking-widest'>
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Minh</p>
+      <Username />
     </header>
   )
 }
