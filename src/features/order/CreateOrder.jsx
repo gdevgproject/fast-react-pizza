9ef95f4d8192d0f71dffc61fd1dd266c520ed1bb
gdevgproject <gdev.gproject@gmail.com>
2025-01-26 23:51:47 +0700

    8.5.2 Styling Button: Element States and Transitions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -79,7 +79,12 @@ function CreateOrder() {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order ....' : 'Order now'}</button>
+          <button
+            className='rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Placing order ....' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
