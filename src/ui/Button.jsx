45556cd1c8575b5f8ac79bec2c0cd0bca23d4fc7
gdevgproject <gdev.gproject@gmail.com>
2025-01-29 23:27:19 +0700

    8.7.2 Styling the cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,7 +6,9 @@ export default function Button({ children, disabled, to, type = 'primary' }) {
 
   const styles = {
     primary: base + 'px-4 py-3 sm:px-6 sm:py-4',
-    small: base + 'px-4 py-2 sm:px-5 sm:py-2.5 text-xs'
+    small: base + 'px-4 py-2 sm:px-5 sm:py-2.5 text-xs',
+    secondary:
+      'rounded-full font-semibold tracking-wide text-stone-400 border-2 border-stone-300 uppercase transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:ring focus:ring-stone-300 hover:text-stone-800 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-4 py-2.5 sm:px-6 sm:py-3.5 focus:text-stone-800'
   }
 
   if (to)
