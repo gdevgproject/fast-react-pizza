b88cddeab263700e736f52c3c6d251e05148a8b1
gdevgproject <gdev.gproject@gmail.com>
2025-01-30 03:22:12 +0700

    8.9 Styling the order overview

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -35,7 +35,7 @@ function Cart() {
 
       <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
 
-      <ul className='mt-3 divide-y divide-stone-200 border-b'>
+      <ul className='mt-3 divide-y divide-stone-200 border-b border-stone-200'>
         {cart.map((item) => (
           <CartItem item={item} key={cart.pizzaId} />
         ))}
