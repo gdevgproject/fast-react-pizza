621fb7c597070afa8979102b220ca7455de26b16
gdevgproject <gdev.gproject@gmail.com>
2025-02-01 12:34:35 +0700

    9.0 Modeling "User" state with redux toolkit

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,3 +1,8 @@
+import { useSelector } from 'react-redux'
+import { selectUser } from './userSlice'
+
 export default function Username() {
-  return <div className='hidden text-sm font-semibold md:block'>Minh</div>
+  const username = useSelector(selectUser)
+  if (!username) return null
+  return <div className='hidden text-sm font-semibold md:block'>{username}</div>
 }
